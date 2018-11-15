86041048aaf0ac1203033eda6ab9e13d0679c1bb
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-15 22:19:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function  sayHiToGrandma(string){
   if string == ""I love you, Grandma." {
-    return 
+    return "I love you, too."
   }
   if string.toUpperCase() === uppercase{
     return "YES INDEED!"; 
